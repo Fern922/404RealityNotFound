61a922d60ea26bbdf4dd057d5a605bf2fd1dccf1
JoshTabsDEv <tabura.joshua@cec.edu.ph>
2025-07-11 08:09:39 +0800

    fixed promotional

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -316,7 +316,7 @@ const Home = () => {
             >
               <iframe
                 className="absolute top-0 left-0 w-full h-full"
-                src="https://www.youtube.com/watch?v=aBrAFqP-zOg"
+                src="https://www.youtube.com/embed/aBrAFqP-zOg?si=wK_C3EPxbk5j4bVs"
                 title="Bohol Lens Promotional Video"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
